4ca813404470791d10121a12ea0e3c86d1656aa6
Mackenzie Straight <mack.straight@mobilelabsinc.com>
2017-05-26 00:44:28 -0400

    hideously evil linker hack to preserve relocation info

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ GOARCH=386 go tool asm -I ~/code/go_src/go/src/runtime wut.s
 go tool asm -I ~/code/go_src/go/src/runtime wut64.s
 go tool compile -+ -wb=false main.go
 go tool pack c main.a main.o wut.o wut64.o
-go tool link -f -v -multiboot -T 0x101000 -E _load_kernel main.a
+go tool link -f -v -multiboot -T 0x101000 -E _load_kernel -emit-reloc main.a
